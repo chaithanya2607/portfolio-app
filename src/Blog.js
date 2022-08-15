5e282b8ed6a21519efe5c984710f545939137214
chaithanya2607 <rchaithanyaraot@gmail.com>
2022-08-15 15:34:48 +0530

    blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,17 @@
 export function Blog(){
     return(
         <div>
-            <p>blog</p>
+            <div>
+                <ul>
+                    <li>
+                    <a  href="https://medium.com/@rchaithanyaraot/the-4-paths-of-yoga-ac18f39f31b3">BLOG</a>
+                    </li>
+                    <li>
+                    <a  href="https://medium.com/@rchaithanyaraot/the-4-paths-of-yoga-ac18f39f31b3">BLOG</a>
+                    </li>
+                </ul>
+            
+            </div>
         </div>
     )
 }
